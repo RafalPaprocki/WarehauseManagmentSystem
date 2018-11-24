ac06d28714940dc54201f9d49efcac42f24b1799
Rafal Paprocki <rafalpaprocki@o2.pl>
2018-11-24 16:24:16 +0100

    user and role models

---
+++ src/main/java/com/warehausesystem/app/warehauseSystemWeb/model/Role.java
@@ -0,0 +1,4 @@
+package com.warehausesystem.app.warehauseSystemWeb.model;
+
+public class Role {
+}
--- src/main/java/com/warehausesystem/app/warehauseSystemWeb/repository/UserRepository.java
+++ src/main/java/com/warehausesystem/app/warehauseSystemWeb/repository/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface WarehauseRepository extends JpaRepository<User,Long> {
-
-
+public interface UserRepository extends JpaRepository<User,Long> {
+    
 }
